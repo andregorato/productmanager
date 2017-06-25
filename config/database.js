35028e8300b81ba30ecb129024d6d80bb5b876b7
André Gorato <andre.gorato@monkeynapps.com>
2017-06-24 22:36:18 -0300

    configurando mongolab

--- config/database.js
+++ config/database.js
@@ -7,12 +7,15 @@
 
 			db.on('error', console.error.bind(console, 'Não foi possível estabelecer a conexão com MongoDb:\n\n'));
 			db.once('open', function() {
-				console.log('Conexão com MongoDb realizada com sucesso.');
+				console.log('Conexão com MongoDb realizada com sucesso. :-)');
 
 				callback();
 			});
 
-			Mongoose.connect('mongodb://localhost:27017/product-manager');			
+			// Mongoose.connect('mongodb://localhost:27017/product-manager');	
+
+			Mongoose.connect('mongodb://root:02986080@ds151028.mlab.com:51028/product-manager');	
+					
 		}
 	};	
 })();
